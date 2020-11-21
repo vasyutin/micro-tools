b18c8e2b6dcfe8f66397b6661e489d7562fc93b2
sv <svpro@outlook.com>
2020-11-22 00:01:06 +0300

    Mounth and Year mode for NDIR

--- src/ndir/ndir.cpp
+++ src/ndir/ndir.cpp
@@ -9,44 +9,61 @@
 #include <stdio.h>
 
 //---------------------------------------------------------------------------
+enum class TMode {Date, DateTime, MonthYear, Help, Error};
+
+
+//---------------------------------------------------------------------------
 int main(int Argc_, char *Argv_[])
 {
-struct THelper {
-	static bool askingHelp(const char *Arg_) {
-		if(!strcmp(Arg_, "-h") || 
-			!strcmp(Arg_, "-H") ||
-			!strcmp(Arg_, "/?"))
-			return true;
-		return false;
-		}
-
-	// ---
-	static bool addTime(const char *Arg_) {
-		if(!strcmp(Arg_, "-t") || !strcmp(Arg_, "-T"))
-			return true;
-		return false;
+	struct THelper {
+		static void usage() {
+			printf("NDIR. Create directory named as current date (and time)."
+				"Copyright 2000-2020 by Sergey Vasyutin.\n"
+				"Usage:\n"
+				"\tndir <-t>\n"
+				"Switches:\n"
+				"\t-t - add time to directory name;\n"
+				"\t-m - year and month.");
 		}
 	};
 
-if(Argc_ == 2 && THelper::askingHelp(Argv_[1])) {
-	printf("NDIR. Create directory named as current date (and time)."
-		"Copyright 2000-2019 by Sergey Vasyutin.\n"
-		"Usage:\n"
-		"\tndir <-t>\n"
-		"Switches:\n"
-		"\t-t - add time to directory name.");
-	return 0;
+	// ---
+	TMode g_Mode = TMode::Date;
+
+	if(Argc_ == 2) {
+		const char* Arg = Argv_[1];
+		if(!strcmp(Arg, "-h") || !strcmp(Arg, "-H") || !strcmp(Arg, "/?")) {
+			g_Mode = TMode::Help;
+		}
+		else if(!strcmp(Arg, "-t") || !strcmp(Arg, "-T")) {
+			g_Mode = TMode::DateTime;
+		}
+		else if(!strcmp(Arg, "-m") || !strcmp(Arg, "-M")) {
+			g_Mode = TMode::MonthYear;
+		}
+		else {
+			g_Mode = TMode::Error;
+		}
 	}
 
-char DirName[50];
-SYSTEMTIME st;
+	if(TMode::Error == g_Mode) {
+		printf("Invalid switch '%s'.\n", Argv_[1]);
+		THelper::usage();
+		return 1;
+	}
+	if(TMode::Help == g_Mode) {
+		THelper::usage();
+		return 0;
+	}
 
-GetLocalTime(&st);
-GetDateFormatA(LOCALE_SYSTEM_DEFAULT, 0, &st, "yy-MM-dd", DirName, 20);
+	char DirName[64];
+	SYSTEMTIME st;
 
-if(Argc_ == 2 && THelper::addTime(Argv_[1])) {
-   GetTimeFormatA(LOCALE_SYSTEM_DEFAULT, TIME_NOSECONDS | TIME_FORCE24HOURFORMAT,
-		&st, "_HH-mm", DirName + 6, 20);
-   }
-return (CreateDirectoryA(DirName, NULL) != 0)? 0: 1;
+	GetLocalTime(&st);
+	GetDateFormatA(LOCALE_SYSTEM_DEFAULT, 0, &st, TMode::MonthYear == g_Mode? "yy-MM": "yy-MM-dd", DirName, 20);
+	if(TMode::DateTime == g_Mode) {
+		GetTimeFormatA(LOCALE_SYSTEM_DEFAULT, TIME_NOSECONDS | TIME_FORCE24HOURFORMAT,
+			&st, "_HH-mm", DirName + strlen(DirName), 20);
+	}
+	return (CreateDirectoryA(DirName, NULL) != 0)? 0: 1;
 }
